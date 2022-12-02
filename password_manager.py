69ee552f2b5f23d822ce9a78b3d3aeb147463223
aidensane <98586059+aidensane@users.noreply.github.com>
2022-12-02 17:18:13 +0100

    fixed the encryption

--- password_manager.py
+++ password_manager.py
@@ -1,15 +1,28 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("What is the master password? ")
-
-
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)
+        key_file.write(key)'''
 
 
-write_key()
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
 
 
 def view():  # function to view saved passwords.
@@ -18,7 +31,8 @@ def view():  # function to view saved passwords.
             data = line.rstrip()
             user, password = data.split('|')
             # splits data into username and password for easier readability.
-            print("User: ", user, "|", "Password: ", password)
+            print("User: ", user, "|", "Password: ",
+                  fer.decrypt(password.encode()).decode())
 
 
 def add():  # function to add a new password.
@@ -26,7 +40,7 @@ def add():  # function to add a new password.
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + '\n')
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + '\n')
 
 
 while True:
