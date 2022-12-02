3de99009381765a717eeb7b280abc96727914f4a
aidensane <98586059+aidensane@users.noreply.github.com>
2022-12-02 16:11:14 +0100

    defined add and view functions

--- password_manager.py
+++ password_manager.py
@@ -1,4 +1,20 @@
-pwd = input("What is the master password? ")
+master_pwd = input("What is the master password? ")
+
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split('|')
+
+
+def add():
+    name = input("Account name: ")
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + pwd + '\n')
+
 
 while True:
     mode = input(
@@ -7,9 +23,9 @@ while True:
         break
 
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     else:
         print("Invalid mode.")
         continue
